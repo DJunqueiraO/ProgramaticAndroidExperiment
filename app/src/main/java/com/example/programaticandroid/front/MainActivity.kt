c5dc8dbda5d2d26c2aff9f29f41e4171e63ff4c5
Audioteca Ancap <danielpotoco@gmail.com>
2023-01-08 12:07:56 -0300

    Studing other things

--- app/src/main/java/com/example/programaticandroid/front/MainActivity.kt
+++ app/src/main/java/com/example/programaticandroid/front/MainActivity.kt
@@ -1,29 +1,23 @@
 package com.example.programaticandroid.front
 
+import android.content.Context
 import android.content.res.Resources
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
-import android.widget.TextView
+import android.widget.RelativeLayout.LayoutParams
+import com.example.programaticandroid.utils.extensions.viewgroup.addViews
 
 class MainActivity : AppCompatActivity() {
-    lateinit var viewGroup: ViewGroup
-    lateinit var textView: TextView
+    lateinit var viewGroup: MainActivityViewGroup
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        textView = TextView(this).apply {
-            val size = Resources.getSystem().displayMetrics.widthPixels/5
-            val layoutParams = RelativeLayout.LayoutParams(size, size)
-            setBackgroundColor(Color.BLUE)
-            this.layoutParams = layoutParams
-            text = "Lero Lero"
-        }
-        viewGroup = RelativeLayout(this).apply {
-            setBackgroundColor(Color.RED)
-            addView(textView)
-        }
+        viewGroup = MainActivityViewGroup(this)
         setContentView(viewGroup)
     }
 }
