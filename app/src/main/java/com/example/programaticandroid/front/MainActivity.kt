4730148d6987c6602730c2d796fcca04a9149866
Audioteca Ancap <danielpotoco@gmail.com>
2023-01-08 00:24:07 -0300

    Studing other things

--- app/src/main/java/com/example/programaticandroid/front/MainActivity.kt
+++ app/src/main/java/com/example/programaticandroid/front/MainActivity.kt
@@ -5,72 +5,25 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
 import android.widget.RelativeLayout
-import com.example.programaticandroid.utils.extensions.viewgroup.addViews
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewGroup: ViewGroup
-    val createViewGroup: () -> ViewGroup = {
-        val viewGroup = LinearLayout(this)
-        setContentView(viewGroup)
-        viewGroup
-    }
-    lateinit var stoneButton: Button
-    lateinit var paperButton: Button
-    lateinit var scissorsButton: Button
-    lateinit var lizardButton: Button
-    lateinit var spokButton: Button
-    val createButton: (text: String) -> Button = {text ->
-        val button = Button(this)
-        button.text = text
-        button.background = null
-        button.setOnClickListener {
-            if (button.isSelected) {
-                button.isSelected = false
-                button.background = null
-            } else {
-                button.isSelected = true
-                button.setBackgroundColor(Color.RED)
-            }
-        }
-        button
-    }
+    lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initialize()
-        constrain()
-        configure()
-    }
-    private fun initialize() {
-        viewGroup = createViewGroup()
-        stoneButton = createButton("Pedra")
-        paperButton = createButton("Papel")
-        scissorsButton = createButton("Tesour")
-        lizardButton = createButton("Lagart")
-        spokButton = createButton("Spok")
-    }
-    private fun configure() {
-        viewGroup.addViews(
-            arrayOf(
-                stoneButton,
-                paperButton,
-                scissorsButton,
-                lizardButton,
-                spokButton
-            )
-        )
-    }
-    private fun constrain() {
-        val size = Resources.getSystem().displayMetrics.widthPixels/5
-        val layoutParams = RelativeLayout.LayoutParams(size, size)
-        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT)
-        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP)
-        stoneButton.layoutParams = layoutParams
-        paperButton.layoutParams = layoutParams
-        scissorsButton.layoutParams = layoutParams
-        lizardButton.layoutParams = layoutParams
-        spokButton.layoutParams = layoutParams
+        textView = TextView(this).apply {
+            val size = Resources.getSystem().displayMetrics.widthPixels/5
+            val layoutParams = RelativeLayout.LayoutParams(size, size)
+            setBackgroundColor(Color.BLUE)
+            this.layoutParams = layoutParams
+            text = "Lero Lero"
+        }
+        viewGroup = RelativeLayout(this).apply {
+            setBackgroundColor(Color.RED)
+            addView(textView)
+        }
+        setContentView(viewGroup)
     }
 }
